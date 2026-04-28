8bd58886935cca1c5492a1c0eccdd8c4ce9ee576
DiegoFChC <diegofech15@gmail.com>
2026-04-28 09:10:18 -0500

    Update project 10: Deleted

--- 10-mern-auth-jwt/backend/src/config/resend.ts
+++
@@ -1,4 +0,0 @@
-import { Resend } from "resend";
-import { RESEND_API_KEY } from "../constants/env";
-
-export const resend = new Resend(RESEND_API_KEY);
--- 10-mern-auth-jwt/backend/src/routes/user.route.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import { getUserHandler } from '../controllers/user.controller'
-
-export const userRoutes = Router()
-
-// prefix: /user
-userRoutes.get('/', getUserHandler)
